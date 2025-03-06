d6eea3d3c1bff707b0dc2f2d68af103f123535de
dhavalShiv3223 <dhaval.mulasheeya@shivlab.com>
2025-03-06 17:10:19 +0530

    fixed file upload issue

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,20 +1,23 @@
 export function objectToFormData(obj, formData = new FormData(), parentKey = '') {
-    for (let key in obj) {
+  for (let key in obj) {
       if (!obj.hasOwnProperty(key)) continue;
       const value = obj[key];
-      // Construct the form key (e.g., "mappings[AnnuitiesDueList_CPi][Docket Number]")
       const formKey = parentKey ? `${parentKey}[${key}]` : key;
-      if (
-        value &&
-        typeof value === 'object' &&
-        !(value instanceof File) && 
-        !(value instanceof Blob)
-      ) {
-        // Recursively process nested objects (or arrays)
-        objectToFormData(value, formData, formKey);
+
+      if (Array.isArray(value)) {
+          // If the value is an array and contains files, append all files with the same key
+          value.forEach((item) => {
+              if (item instanceof File || item instanceof Blob) {
+                  formData.append(parentKey || key, item);
+              } else {
+                  objectToFormData(item, formData, formKey);
+              }
+          });
+      } else if (value && typeof value === 'object' && !(value instanceof File) && !(value instanceof Blob)) {
+          objectToFormData(value, formData, formKey);
       } else {
-        formData.append(formKey, value);
+          formData.append(formKey, value);
       }
-    }
-    return formData;
   }
+  return formData;
+}
