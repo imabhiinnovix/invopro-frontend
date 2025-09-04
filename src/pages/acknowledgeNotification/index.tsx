a3176ebfd2f070d44c777d3e492f09243cb69c95
AbhishekSharma453 <abhishek.sharma@innovix-labs.com>
2025-09-04 18:09:30 +0530

    acknowledge integrate

--- src/pages/acknowledgeNotification/index.tsx
+++ src/pages/acknowledgeNotification/index.tsx
@@ -1,98 +1,4 @@
-// import Box from "@mui/material/Box";
-// import { Card, CircularProgress, Button, Typography } from "@mui/material";
-// import { STYLE_GUIDE } from "../../styles";
-// import { useParams, useNavigate, useSearchParams } from "react-router-dom";
-// import { useEffect, useState } from "react";
-// import usePut from "../../hooks/usePut";
-// import { PUT } from "../../services/apiRoutes";
 
-// function AcknowledgeNotification() {
-//   const { id } = useParams();
-//   const navigate = useNavigate();
-//   const [searchParams] = useSearchParams();
-// const key = searchParams.get('key');
-//   const [loading, setLoading] = useState(true);
-//   const [message, setMessage] = useState("");
-//   const [success, setSuccess] = useState(false);
-
-//   // Initialize usePut hook
-//   const acknowledgeNotification = usePut(["acknowledgeNotification"]);
-//   console.log("id", id, key);
-
-//   useEffect(() => {
-//     const sendAcknowledgement = async () => {
-//       try {
-//         const response = await acknowledgeNotification.mutateAsync({
-//           url: `${PUT.SEND_ACKNOWLEDGE}/${id}`,
-//           payload: { identifierKey:key },
-//         });
-//         console.log("respose",response)
-
-//         if (response.success) {
-//           setMessage(
-//             response.message || "Notification acknowledged successfully"
-//           );
-//           setSuccess(true);
-//         } else {
-//           throw new Error(response.message || "Acknowledgement failed");
-//         }
-//       } catch (error) {
-//         setMessage(error.message || "Failed to acknowledge notification");
-//         setSuccess(false);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     if (id && key) {
-//       sendAcknowledgement();
-//     }
-//   }, [id, key, acknowledgeNotification]);
-
-//   const handleReturnToLogin = () => {
-//     navigate("/login");
-//   };
-
-//   return (
-//     <Box
-//       display="flex"
-//       alignItems="center"
-//       justifyContent="center"
-//       sx={{ margin: STYLE_GUIDE.SPACING.s3 }}
-//     >
-//       <Card sx={{ p: 4, textAlign: "center", maxWidth: 800, pt: 9 }}>
-//         {loading ? (
-//           <>
-//             <CircularProgress size={60} />
-//             <Typography variant="h6" sx={{ mt: 2 }}>
-//               Processing your request...
-//             </Typography>
-//           </>
-//         ) : (
-//           <>
-//             <Typography
-//               variant="h6"
-//               color={success ? "success.main" : "error.main"}
-//               sx={{ mb: 3 }}
-//             >
-//               {message}
-//             </Typography>
-//             <Button
-//               variant="contained"
-//               //   color="primary"
-//               onClick={handleReturnToLogin}
-//               sx={{ mt: 2 }}
-//             >
-//               Return to Login Page
-//             </Button>
-//           </>
-//         )}
-//       </Card>
-//     </Box>
-//   );
-// }
-
-// export default AcknowledgeNotification;
 
 
 import Box from "@mui/material/Box";
@@ -112,16 +18,12 @@ function AcknowledgeNotification() {
   const [message, setMessage] = useState("");
   const [success, setSuccess] = useState(false);
   
-  // Initialize usePut hook
   const acknowledgeNotification = usePut(["acknowledgeNotification"]);
   
-  // Use a ref to track if the API call has been made
   const apiCalled = useRef(false);
   
-  console.log("id", id, key);
 
   useEffect(() => {
-    // Prevent multiple API calls
     if (apiCalled.current || !id || !key) {
       if (!id || !key) {
         setLoading(false);
