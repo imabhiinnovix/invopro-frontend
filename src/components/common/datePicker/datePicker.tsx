340cb817056a59f20ba66dd49f742e1fbb3994e2
AbhishekSharma453 <abhishek.sharma@innovix-labs.com>
2025-10-01 13:30:57 +0530

    update commmon date picker

--- src/components/common/datePicker/datePicker.tsx
+++ src/components/common/datePicker/datePicker.tsx
@@ -1,3 +1,87 @@
+// import { useState } from "react";
+// import {
+//   Controller,
+//   Control,
+//   FieldValues,
+//   Path,
+//   PathValue,
+//   RegisterOptions,
+// } from "react-hook-form";
+// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+// import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
+// import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+// import { DateView } from "@mui/x-date-pickers/models";
+// import { DateTime } from "luxon";
+// import { Theme } from "@mui/material";
+// import { SxProps } from "@mui/material";
+
+// interface CommonDatePickerProps<T extends FieldValues> {
+//   control: Control<T>;
+//   name: Path<T>;
+//   label: string;
+//   views: readonly DateView[];
+//   defaultValue?: PathValue<T, Path<T>>;
+//   rules?: RegisterOptions<T>;
+//   disabled?: boolean;
+//   sx?: SxProps<Theme>;
+// }
+
+// const CommonDatePicker = <T extends FieldValues>(
+//   props: CommonDatePickerProps<T>
+// ) => {
+//   const {
+//     control,
+//     name,
+//     label,
+//     views,
+//     defaultValue = null,
+//     rules = {},
+//     disabled = false,
+//     sx,
+//   } = props;
+
+//   const [tempDate, setTempDate] = useState<DateTime | null>(
+//     defaultValue ? DateTime.fromISO(defaultValue as string) : null
+//   );
+
+//   return (
+//     <Controller
+//       name={name}
+//       control={control}
+//       defaultValue={(defaultValue as PathValue<T, Path<T>>) || undefined}
+//       rules={rules}
+//       render={({ field: { onChange, value }, fieldState: { error } }) => {
+//         const currentValue = value ? DateTime.fromISO(value as string) : null;
+
+//         return (
+//           <LocalizationProvider dateAdapter={AdapterLuxon}>
+//             <MobileDatePicker
+//               label={label}
+//               views={views}
+//               value={tempDate || currentValue || null}
+//               onChange={(date) => setTempDate(date)}
+//               disabled={disabled}
+//               closeOnSelect={false}
+//               onAccept={() => onChange(tempDate?.toISO())}
+//               onClose={() => setTempDate(null)}
+//               slotProps={{
+//                 textField: {
+//                   error: !!error,
+//                   helperText: error?.message,
+//                 },
+//               }}
+//               sx={sx}
+//             />
+//           </LocalizationProvider>
+//         );
+//       }}
+//     />
+//   );
+// };
+
+// export default CommonDatePicker;
+
+
 import { useState } from "react";
 import {
   Controller,
@@ -24,6 +108,7 @@ interface CommonDatePickerProps<T extends FieldValues> {
   rules?: RegisterOptions<T>;
   disabled?: boolean;
   sx?: SxProps<Theme>;
+  disableFuture?: boolean; 
 }
 
 const CommonDatePicker = <T extends FieldValues>(
@@ -38,6 +123,7 @@ const CommonDatePicker = <T extends FieldValues>(
     rules = {},
     disabled = false,
     sx,
+    disableFuture = false, 
   } = props;
 
   const [tempDate, setTempDate] = useState<DateTime | null>(
@@ -61,6 +147,8 @@ const CommonDatePicker = <T extends FieldValues>(
               value={tempDate || currentValue || null}
               onChange={(date) => setTempDate(date)}
               disabled={disabled}
+              disableFuture={disableFuture}
+              maxDate={disableFuture ? DateTime.now() : undefined}
               closeOnSelect={false}
               onAccept={() => onChange(tempDate?.toISO())}
               onClose={() => setTempDate(null)}
