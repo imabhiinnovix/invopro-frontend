b77ff3ae465e844714458e1f3c6d980721d6b960
Abhishek Singh <abhishek@innovix-labs.com>
2025-11-06 11:48:30 +0530

    permission changes

--- src/pages/notivixDashboard/components/NotivixChartGrid.tsx
+++ src/pages/notivixDashboard/components/NotivixChartGrid.tsx
@@ -646,7 +646,7 @@ export const ChartGrid: React.FC<ChartGridProps> = ({
             dashboardId,
             dashboardType: currentDashboard?.settings?.dashboardType,
             filters: dashboardFilters,
-=          })
+          })
         );
       }
     }
