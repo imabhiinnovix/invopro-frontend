dc7c9c5f00313bf8efef47e776f4add3b6c5e724
abdul malik ismail <abdul@vonic.ai>
2025-11-12 15:52:51 +0530

    merge conflict fix

--- src/pages/validationErrors/ValidationErrorsDataTable.tsx
+++ src/pages/validationErrors/ValidationErrorsDataTable.tsx
@@ -35,139 +35,6 @@ const CustomFooter = ({
   );
 };
 
-
-
-interface ValidationErrorsDataTableProps {
-  rows: any[];
-  paginationModel: { page: number; pageSize: number };
-  setPaginationModel: (model: { page: number; pageSize: number }) => void;
-  rowCount: number;
-  validationErrorList?: any;
-  isLatest?: boolean;
-}
-
-export const ValidationErrorsDataTable: React.FC<
-  ValidationErrorsDataTableProps
-> = ({
-  rows,
-  paginationModel,
-  setPaginationModel,
-  rowCount,
-  validationErrorList,
-  isLatest
-}) => {
-
-  const columns: GridColDef[] = [
-  {
-    id: "fileRowNumber",
-    label: "Row Number",
-    width: 140,
-    disableColumnMenu: true,
-    resizable: true,
-  },
-  {
-    id: "fileName",
-    label: "File Name",
-    width: 200,
-    disableColumnMenu: true,
-    resizable: true,
-  },
-  {
-    id: "errorMessage",
-    label: "Error Message",
-    width: 300,
-    disableColumnMenu: true,
-    resizable: true,
-  },
-  {
-    id: "fileAttributeValue",
-    label: "Attribute Value",
-    width: 150,
-    disableColumnMenu: true,
-    resizable: true,
-  },
-  {
-    id: "status",
-    label: "Status",
-    width: 100,
-    disableColumnMenu: true,
-    resizable: true,
-    renderCell: (row: Record<string, unknown>) => {
-      let chipColor = "default";
-      let chipVariant = "outlined";
-
-      if (row.status === "resolved") {
-        chipColor = "success";
-      } else if (row.status === "open") {
-        chipColor = "primary";
-        chipVariant = "outlined"; // This will give us the blue border
-      } else {
-        chipColor = "error";
-      }
-      // console.log("row", row);
-
-      return (
-        <Chip
-          label={row.status || "Unknown"}
-          size="small"
-          color={chipColor}
-          variant={chipVariant}
-        />
-      );
-    },
-  },
-  {
-    id: "actions",
-    label: "Actions",
-    width: 150,
-    disableColumnMenu: true,
-    sortable: false,
-    resizable: false,
-    renderCell: (row: Record<string, unknown>) => {
-      const isDiscarded = row.status === "discarded";
-      const isResolved = row.status === "resolved";
-
-      return (
-        <Box sx={{ display: "flex", gap: 1 }}>
-          {row.errorCode === "1005" ? (
-            <Tooltip title="Resolve" arrow>
-              <Button
-                variant="text"
-                onClick={() => row.handleResolve(row)}
-                sx={{ minWidth: "auto" }}
-                disabled={isLatest === false}
-              >
-                <FileDownloadDoneIcon />
-              </Button>
-            </Tooltip>
-          ) : (
-            <Tooltip title="Take Action" arrow>
-              <Button
-                variant="text"
-                onClick={() => row.handleEdit(row)}
-                sx={{ minWidth: "auto" }}
-                disabled={isResolved || isLatest === false}
-              >
-                <SwipeUpIcon />
-              </Button>
-            </Tooltip>
-          )}
-          <Tooltip title="Discard" arrow>
-            <Button
-              variant="text"
-              onClick={() => row.handleDiscard(row)}
-              sx={{ minWidth: "auto" }}
-              disabled={isDiscarded || isResolved || isLatest === false}
-            >
-              <RemoveCircleIcon />
-            </Button>
-          </Tooltip>
-        </Box>
-      );
-    },
-  },
-];
-
 interface ValidationErrorsDataTableProps {
   rows: any[];
   paginationModel: { page: number; pageSize: number };
@@ -177,6 +44,7 @@ interface ValidationErrorsDataTableProps {
   handleDiscardSelectedRows?: (selectedRows: any[]) => void;
   resetSelections: boolean;
   setResetSelections: (reset: boolean) => void;
+  isLatest?: boolean;
 }
 
 export const ValidationErrorsDataTable: React.FC<
@@ -190,8 +58,120 @@ export const ValidationErrorsDataTable: React.FC<
   handleDiscardSelectedRows,
   resetSelections,
   setResetSelections,
+  isLatest,
 }) => {
   const tableRef = useRef(null);
+
+  const columns: GridColDef[] = [
+    {
+      id: "fileRowNumber",
+      label: "Row Number",
+      width: 140,
+      disableColumnMenu: true,
+      resizable: true,
+    },
+    {
+      id: "fileName",
+      label: "File Name",
+      width: 200,
+      disableColumnMenu: true,
+      resizable: true,
+    },
+    {
+      id: "errorMessage",
+      label: "Error Message",
+      width: 300,
+      disableColumnMenu: true,
+      resizable: true,
+    },
+    {
+      id: "fileAttributeValue",
+      label: "Attribute Value",
+      width: 150,
+      disableColumnMenu: true,
+      resizable: true,
+    },
+    {
+      id: "status",
+      label: "Status",
+      width: 100,
+      disableColumnMenu: true,
+      resizable: true,
+      renderCell: (row: Record<string, unknown>) => {
+        let chipColor = "default";
+        let chipVariant = "outlined";
+
+        if (row.status === "resolved") {
+          chipColor = "success";
+        } else if (row.status === "open") {
+          chipColor = "primary";
+          chipVariant = "outlined"; // This will give us the blue border
+        } else {
+          chipColor = "error";
+        }
+        // console.log("row", row);
+
+        return (
+          <Chip
+            label={row.status || "Unknown"}
+            size="small"
+            color={chipColor}
+            variant={chipVariant}
+          />
+        );
+      },
+    },
+    {
+      id: "actions",
+      label: "Actions",
+      width: 150,
+      disableColumnMenu: true,
+      sortable: false,
+      resizable: false,
+      renderCell: (row: Record<string, unknown>) => {
+        const isDiscarded = row.status === "discarded";
+        const isResolved = row.status === "resolved";
+
+        return (
+          <Box sx={{ display: "flex", gap: 1 }}>
+            {row.errorCode === "1005" ? (
+              <Tooltip title="Resolve" arrow>
+                <Button
+                  variant="text"
+                  onClick={() => row.handleResolve(row)}
+                  sx={{ minWidth: "auto" }}
+                  disabled={isLatest === false}
+                >
+                  <FileDownloadDoneIcon />
+                </Button>
+              </Tooltip>
+            ) : (
+              <Tooltip title="Take Action" arrow>
+                <Button
+                  variant="text"
+                  onClick={() => row.handleEdit(row)}
+                  sx={{ minWidth: "auto" }}
+                  disabled={isResolved || isLatest === false}
+                >
+                  <SwipeUpIcon />
+                </Button>
+              </Tooltip>
+            )}
+            <Tooltip title="Discard" arrow>
+              <Button
+                variant="text"
+                onClick={() => row.handleDiscard(row)}
+                sx={{ minWidth: "auto" }}
+                disabled={isDiscarded || isResolved || isLatest === false}
+              >
+                <RemoveCircleIcon />
+              </Button>
+            </Tooltip>
+          </Box>
+        );
+      },
+    },
+  ];
 
   // Conditionally render the footer only when validationErrorList.data is available
   const renderFooter = () => {
